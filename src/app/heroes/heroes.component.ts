181498b27632e655db41d10b1083228711d91aab
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-04-07 09:56:39 +0200

    The Hero Editor - view to edit the hero details

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.sass']
+})
+export class HeroesComponent implements OnInit {
+
+  hero:Hero = {
+    id:1,
+    name:'windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
