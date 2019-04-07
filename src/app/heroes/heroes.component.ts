0a31e69fae1098d1376403651c42f617439e0610
mohamed_sabri <mohamed.sabri@visionvalley.net>
2019-04-07 11:11:07 +0200

    Displaying heroes and add styles to the heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,15 @@
+import { HEROES } from './../mock-heroes';
 import { Hero } from '../hero';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.sass']
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+  heroes = HEROES ;
 
   hero:Hero = {
     id:1,
